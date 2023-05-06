dd298e629722c246bcaf400ae2a3710f81cf68c0
jiinkyung <jinkyung010310@gmail.com>
2023-05-07 01:12:30 +0900

    상영작 후기 작성 및 조회

--- dsfest_BE/dsfest_BE/urls.py
+++ dsfest_BE/dsfest_BE/urls.py
@@ -25,4 +25,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('festapp.urls')),
     path('', include('event.urls')),
+    path('', include('reviews.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
