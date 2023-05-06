984d432a3186372ecf5f32aff6881cfe00c2da7e
kknari <nari907@duksung.ac.kr>
2023-05-07 00:53:22 +0900

    낭만에 대하여 완료git add .!

--- dsfest_BE/dsfest_BE/settings.py
+++ dsfest_BE/dsfest_BE/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'festapp',
     'rest_framework',
+    'event',
 ]
 
 MIDDLEWARE = [
--- dsfest_BE/event/urls.py
+++ dsfest_BE/event/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('abouts/', views.PostList.as_view()),
+    path('event/', views.PostList.as_view()),
 ]
